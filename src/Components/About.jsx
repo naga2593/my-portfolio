6a00592befb1a6a5353f5202503a325f8c4f88dd
nagaraju <nagaraju2593@gmail.com>
2023-08-18 14:19:59 +0530

    experience page implementation

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about-page">
+      <p className="about-text">
+      As a front-end developer, I am passionate about creating visually appealing and user-friendly websites
+       and web applications. With a keen eye for design and a strong understanding of user experience principles, I strive to build interfaces that not only look great but also provide seamless interactions for users..
+      </p>
+
+      <br />
+
+      <p className="text2">
+       I have honed my skills in HTML, CSS, and JavaScript, staying up-to-date with the latest front-end frameworks and libraries to keep pace with the ever-evolving web landscape.
+      I believe that collaboration and communication are essential for delivering successful projects. Working closely with designers, back-end developers, and other stakeholders, I ensure that the vision for a website or application is translated into a functional and visually appealing reality.
+      </p>
+  </div>
   )
 }
 
--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -1,8 +1,90 @@
-import React from 'react'
+import React from 'react';
+import html from "../Assets/html.png";
+import css from "../Assets/css.png";
+import javascript from "../Assets/javascript.png";
+import reactImage from "../Assets/react.png";
+import nextjs from "../Assets/nextjs.png";
+import github from "../Assets/github.png";
+import tailwind from "../Assets/tailwind.png";
+import antd from "../Assets/antd.png";
+import angularjs from "../Assets/angularjs.png";
+
 
  const Experience = () => {
+  const technologies = [
+    {
+      id: 1,
+      src: html,
+      title: "HTML",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 2,
+      src: css,
+      title: "CSS",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 3,
+      src: javascript,
+      title: "JavaScript",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 4,
+      src: angularjs,
+      title: "Angular JS",
+      style: "shadow-red-600",
+    },
+    {
+      id: 5,
+      src: reactImage,
+      title: "React",
+      style: "shadow-blue-600",
+    },
+    {
+      id: 5,
+      src: nextjs,
+      title: "Next JS",
+      style: "shadow-white",
+    },
+    {
+      id: 6,
+      src: antd,
+      title: "Ant D",
+      style: "shadow-blue-400",
+    },
+    {
+      id: 7,
+      src: tailwind,
+      title: "Tailwind",
+      style: "shadow-sky-400",
+    },
+   
+    {
+      id: 8,
+      src: github,
+      title: "GitHub",
+      style: "shadow-gray-400",
+    },
+  ];
   return (
-    <div>Experience</div>
+    <>
+     <div class="container">
+     <div className='row'>
+    
+          {technologies?.map(({ id, src, title, style }) => (
+            <div
+              key={id}
+              className='col-sm-4 thumbnail'
+            >
+              <img src={src} alt="" />
+              <p>{title}</p>
+            </div>
+          ))}
+          </div>
+          </div>
+    </>
   )
 }
 export default Experience;
